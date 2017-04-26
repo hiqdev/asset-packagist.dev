229906b1f74b969f92162cc755fa0bda7bd8cf26
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-04-26 17:51:48 +0300

    Added console controllers to config

--- src/config/hidev.php
+++ src/config/hidev.php
@@ -4,5 +4,13 @@ return [
     'controllerMap' => [
         'queue' => hiqdev\assetpackagist\console\QueueController::class,
         'asset-package' => hiqdev\assetpackagist\console\AssetPackageController::class,
+        'maintenance' => \hiqdev\assetpackagist\console\MaintenanceController::class,
+        'migrate' => [
+            'class' => \yii\console\controllers\MigrateController::class,
+            'migrationNamespaces' => [
+                'hiqdev\assetpackagist\migrations'
+            ],
+            'migrationPath' => null
+        ]
     ]
 ];
